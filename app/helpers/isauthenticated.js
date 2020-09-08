410952195bea651857fe5a4f0da369414eef8ec8
yanr <yan.rotinsulu@gmail.com>
2020-09-08 23:17:08 +0700

    implement isauthenticated middleware

---
+++ app/helpers/isauthenticated.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const passportConfig = require('../configs/passport-config');
+
+let isAuthenticatedFunction;
+
+switch (passportConfig.strategy){
+    case "local":
+        isAuthenticatedFunction = function isAuthenticated(req, res, next) {
+            if (req.isAuthenticated())
+                return next();
+            res.json({'status': 'you are not authenticated'});
+        }
+    // jwt is default
+    default:
+        isAuthenticatedFunction = function isAuthenticated(req, res, next) {
+            passport.authenticate('jwt', {session:false}, function(err,user){
+                if(err || !user){
+                    res.status(401).json({'message': 'you are not authenticated'});
+                }
+                else{
+                    res.locals.test = true;
+                    next();
+                }
+            })(req,res);
+        }
+}
+
+module.exports = isAuthenticatedFunction;
--- app/routes/auths.js
+++ app/routes/auths.js
@@ -4,66 +4,49 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 const authService = require('../services/auth_services');
-const jwtConfig = require('../configs/jwt-config');
+const passportConfig = require('../configs/passport-config');
+const isAuthenticated = require('../helpers/isauthenticated');
 
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated())
-      return next();
-    res.json({'status': 'you are not authenticated'});
+switch(passportConfig.strategy){
+  case "local":
+
+    router.post('/login', 
+      passport.authenticate('local', { failureRedirect: '/auths/login' }),
+      function(req, res) {
+        res.redirect('/');
+      }
+    );
+
+    router.get('/logout', function(req, res){
+      req.logout();
+      res.redirect('/auths/login');
+    });
+  
+  default:
+
+    router.post('/login', async function(req, res) {
+        UserID = await authService.getUserId(req.body.username, req.body.password);
+        if (UserID){
+          const token = jwt.sign({ id : UserID }, passportConfig.secretKey, {expiresIn: 3600});
+          res.status(200).send({
+            token: token,
+            message: 'user found and logged in'
+          });
+        }
+        else{
+          res.status(401).redirect('/');
+        }
+      }
+    );
 }
 
-//with local strategy
-router.post('/login-local', 
-    passport.authenticate('local', { failureRedirect: '/' }),
-    function(req, res) {
-        console.log("OK", req);
-        res.redirect('/auths/check');
-    }
-);
-
-//with jwt strategy
-router.post('/login', async function(req, res) {
-      UserID = await authService.getUserId(req.body.username, req.body.password);
-      if (UserID){
-        console.log("OK");
-        const token = jwt.sign({ id : UserID }, jwtConfig.secretKey);
-        res.status(200).send({
-          authorized: true,
-          token: token,
-          message: 'user found ang logged in'
-        });
-      }
-      else{
-        res.status(401).redirect('/');
-      }
-    }
-);
-
-
-router.get('/login', function(req, res){
-    res.json({'status': 'ini get login'});
+router.get('/check', isAuthenticated, function(req, res) {
+  res.status(200).json({'message': 'you are authenticated'});
 });
 
-router.get('/logout-local', function(req, res){
-    req.logout();
-    res.redirect('/');
-});
-
-router.get('/check-local', isAuthenticated, function(req, res) {
-    res.json({'status': 'you are authenticated'});
-});
-
-router.get('/check', passport.authenticate('jwt', {session:false}), function(req, res) {
-  res.json({'status': 'your JWT is authenticated'});
-});
-
-router.post('/login-debug',
-  // wrap passport.authenticate call in a middleware function
+router.post('/debug',
   function (req, res, next) {
-    // call passport authentication passing the "local" strategy name and a callback function
-    passport.authenticate('jwt', function (error, user, info) {
-      // this will execute in any case, even if a passport strategy will find an error
-      // log everything to console
+    passport.authenticate(passportConfig.strategy, function (error, user, info) {
       console.log(req);
       console.log(error);
       console.log(user);
@@ -76,10 +59,8 @@ router.post('/login-debug',
       } else {
         next();
       }
-      // res.status(401).send(error);
     })(req, res);
   },
-
   // function to call once successfully authenticated
   function (req, res) {
     res.status(200).send('logged in!');
